c0fc2891347758e9f37502669ec369d2a9c85c74
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 20:54:05 +0100

    added imports and structure

---
+++ src/resolvers/index.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const fsPromises = require('fs/promises')
+const { fileExists, deleteFile, getDirectoryFileNames } = require('../utils/fileHandling')
+const { GraphQLError } = require('graphql')
+
+exports.resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+        
+    }
+}
