eea06a494ee0ce15f97b1a06f6c775b4029b4684
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 21:01:04 +0100

    added readFile

--- src/utils/filehandling.js
+++ src/utils/filehandling.js
@@ -1,5 +1,7 @@
 const fsPromises = require('fs/promises')
 
+exports.readFile = async (filePath) => await fsPromises.readFile(filePath)
+
 exports.fileExists = async (filePath) => !!(await fsPromises.stat(filePath).catch((e) => false))
 
 exports.deleteFile = async (filePath) => await fsPromises.unlink(filePath)
