581700496c3858ea1019c6bd1e95f3d10f8906e5
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 22:13:07 +0100

    added getAllTodos resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const fsPromises = require('fs/promises')
 const { fileExists, deleteFile, getDirectoryFileNames, readFile } = require('../utils/fileHandling')
 const { GraphQLError } = require('graphql')
+const { get } = require('http')
 
 const todosDirectory = path.join(__dirname, '..', 'data', 'todos')
 
@@ -17,7 +18,15 @@ exports.resolvers = {
             return todoData
         },
         getAllTodos: async (_, args, context) => {
-            
+            const todoFiles = await getDirectoryFileNames(todosDirectory)
+            const todos = []
+            for(const file of todoFiles) {
+                const filePath = path.join(todosDirectory, `${file}`)
+                const todoData = JSON.parse(await readFile(filePath))
+                todos.push(todoData)
+            }
+           
+            return todos
         }
 
     },
