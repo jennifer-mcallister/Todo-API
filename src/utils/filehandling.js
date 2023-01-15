8c17c5b1fff67c64496a1362efc5c32b79adf381
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 20:47:01 +0100

    added "delteFile" and "getDirectory"

--- src/utils/filehandling.js
+++ src/utils/filehandling.js
@@ -2,3 +2,7 @@ const fsPromises = require('fs/promises')
 
 exports.fileExists = async (filePath) => !!(await fsPromises.stat(filePath).catch((e) => false))
 
+exports.deleteFile = async (filePath) => await fsPromises.unlink(filePath)
+
+exports.getDirectoryFileNames = async (directoryPath) => await fsPromises.readdir(directoryPath)
+
