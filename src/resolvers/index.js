0b0e10cfb6fdd0a98bd99913b36cde7d1584ccfe
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 21:39:46 +0100

    added getTodoById resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,11 +1,23 @@
 const path = require('path')
 const fsPromises = require('fs/promises')
-const { fileExists, deleteFile, getDirectoryFileNames } = require('../utils/fileHandling')
+const { fileExists, deleteFile, getDirectoryFileNames, readFile } = require('../utils/fileHandling')
 const { GraphQLError } = require('graphql')
+
+const todosDirectory = path.join(__dirname, '..', 'data', 'todos')
 
 exports.resolvers = {
     Query: {
         getTodoById: async (_, args, context) => {
+            const id = args.id
+            const filePath = path.join(todosDirectory, `${id}.json`)
+
+            const todoExists = await fileExists(filePath)
+
+            if (!todoExists) return GraphQLError('Todo has aldready left')
+            
+            const todoData = JSON.parse(await readFile(filePath))
+
+            return todoData
 
         },
         getAllTodos: async (_, args, context) => {
