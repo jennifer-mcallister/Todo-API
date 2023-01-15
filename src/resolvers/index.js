67a3ebca85f48ea5a5f4cd256b6809cc05c867f4
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 20:56:41 +0100

    added structure in Query and Mutation

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,9 +5,24 @@ const { GraphQLError } = require('graphql')
 
 exports.resolvers = {
     Query: {
+        getTodoById: async (_, args, context) => {
+
+        },
+        getAllTodos: async (_, args, context) => {
+            
+        }
 
     },
     Mutation: {
-        
+        createTodo: async (_, args, context) => {
+
+        },
+        updateTodo: async (_, args, context) => {
+
+        },
+        deleteTodo: async (_, args, context) => {
+
+        },
+
     }
 }
