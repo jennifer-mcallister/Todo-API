9a684652ee8774ac933a55c8cc5b6087e9775f97
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-15 20:28:52 +0100

    added funktion to run server

--- src/server.js
+++ src/server.js
@@ -5,3 +5,21 @@ const { loadFiles } = require('@graphql-tools/load-files')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 const path = require('path')
 const { startStandaloneServer } = require('@apollo/server/standalone') 
+
+
+async function run() {
+    try {
+        const typeDefs = await loadFiles(path.join(__dirname, "schema.graphql"))
+        const schema = makeExecutableSchema({
+            typeDefs: typeDefs,
+            resolvers: resolvers
+        })
+        const server = new ApolloServer({ schema: schema})
+        const res = await startStandaloneServer(server)
+        console.log(`Server is up and going ${res.url}`)
+    } catch (error) {
+        console.error(error)   
+    }
+}
+
+run ()
