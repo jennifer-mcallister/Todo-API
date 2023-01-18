8c4765be24415a8faf5d49532eccbe99dacc78fd
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-18 16:35:33 +0100

    Added createTicket

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,6 +5,7 @@ const { GraphQLError } = require('graphql')
 const crypto = require('crypto')
 
 const todosDirectory = path.join(__dirname, '..', 'data', 'todos')
+const ticketDirectory = path.join(__dirname,'..', 'tickets')
 
 exports.resolvers = {
     Query: {
@@ -51,8 +52,8 @@ exports.resolvers = {
         updateTodo: async (_, args, context) => {
             const { id, task, description, done } = args
             const filePath = path.join(todosDirectory, `${id}.json`)
-            const Exists = await fileExists(filePath)
-            if (!Exists) return new GraphQLError('Oppsie that todo does not exist')
+            const exists = await fileExists(filePath)
+            if (!exists) return new GraphQLError('Oppsie that todo does not exist')
             const updatedTodo = {id, task, description, done}
             await fsPromises.writeFile(filePath, JSON.stringify(updatedTodo))
 
@@ -76,6 +77,39 @@ exports.resolvers = {
                 success: true
             }
         },
+        createTicket: async (_,args, context) => {
+            const { title, description, type, priority, status, todoId } = args.input
+            const filePath = path.join(todosDirectory, `${todoId}.json`)
+            const exists = await fileExists(filePath)
+
+            if(!exists) return new GraphQLError('Oppsie that todo does not exist')
+
+            const newTicket = {
+                id: crypto.randomUUID(),
+				title,
+				description: description || '',
+				type,
+				priority: priority || ticketPriority.LOW,
+				status: ticketStatus.NEW,
+				todoId
+            }
+
+            let ticketIdExists = true
+            while (ticketIdExists) {
+                const ticketFilePath = path.join(ticketDirectory, `${id}.json`)
+                const exists = await fileExists(ticketFilePath)
+
+                if(exists) {
+                    newTicket.id = crypto.randomUUID()
+                    filePath = path.join(ticketDirectory, `${newTicket.id}.json`)
+                }
+                idExists = false
+            }
+            await fsPromises.writeFile(filePath, JSON.stringify(newTicket))
+
+            return newTicket 
+
+        }
 
     }
 }
